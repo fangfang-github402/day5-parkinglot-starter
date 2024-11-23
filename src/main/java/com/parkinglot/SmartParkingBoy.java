efcf303b7ad9a95acd1a2275d73044b59006773b
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-23 23:05:15 +0800

    refactor:reformat

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -2,12 +2,12 @@ package com.parkinglot;
 
 import java.util.Comparator;
 
-public class SmartParkingBoy extends ParkingBoy{
+public class SmartParkingBoy extends ParkingBoy {
 
     @Override
     public Ticket park(Car car) {
         ParkingLot parkingLot = parkingLots.stream().max(Comparator.comparing(ParkingLot::getNumberOfEmptyPositions)).orElse(null);
-        if(parkingLot == null){
+        if (parkingLot == null) {
             throw new NoAvailablePositionExpection();
         }
         System.out.println("The car has parked in ParkingLot:" + (parkingLots.indexOf(parkingLot) + 1));
--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -17,8 +17,9 @@ public class SmartParkingBoyTest {
     public void setup() {
         System.setOut(new PrintStream(outContent));
     }
+
     @Test
-    void should_in_firstParkingLot_when_park_given_two_parking_lots_and_with_same_number_of_empty_positions(){
+    void should_in_firstParkingLot_when_park_given_two_parking_lots_and_with_same_number_of_empty_positions() {
         //Given
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
         ParkingLot firstParkingLot = new ParkingLot();
@@ -31,76 +32,76 @@ public class SmartParkingBoyTest {
         //Then
         assertNotNull(ticket);
         Assertions.assertThat(systemOut()).contains("The car has parked in ParkingLot:1");
-     }
-    
-     @Test
-     void should_in_second_parking_lot_when_park_given_second_one_has_more_empty_positions(){
-         //Given
-         SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
-         ParkingLot firstParkingLot = new ParkingLot();
-         ParkingLot secondParkingLot = new ParkingLot();
-         smartParkingBoy.addParkingLot(firstParkingLot);
-         smartParkingBoy.addParkingLot(secondParkingLot);
-         smartParkingBoy.park(new Car());
-         Car car = new Car();
-         //When
-         Ticket ticket = smartParkingBoy.park(car);
-         //Then
-         assertNotNull(ticket);
-         Assertions.assertThat(systemOut()).contains("The car has parked in ParkingLot:2");
-      }
+    }
 
-      @Test
-      void should_return_right_car_when_fetch_given_a_parked_car_and_two_parking_tickets(){
-          //Given
-          SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
-          ParkingLot firstParkingLot = new ParkingLot();
-          ParkingLot secondParkingLot = new ParkingLot();
-          smartParkingBoy.addParkingLot(firstParkingLot);
-          smartParkingBoy.addParkingLot(secondParkingLot);
-          Car car = new Car();
-          Ticket firstTicket = smartParkingBoy.park(car);
-          Car firstFetchedCar = smartParkingBoy.fetch(firstTicket);
-          smartParkingBoy.park(new Car());
-          Ticket secondTicket = smartParkingBoy.park(car);
-          Car secondFetchedCar = smartParkingBoy.fetch(secondTicket);
-          //When
-          //Then
-          assert (car.equals(firstFetchedCar));
-          assert (car.equals(secondFetchedCar));
-       }
+    @Test
+    void should_in_second_parking_lot_when_park_given_second_one_has_more_empty_positions() {
+        //Given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        smartParkingBoy.addParkingLot(firstParkingLot);
+        smartParkingBoy.addParkingLot(secondParkingLot);
+        smartParkingBoy.park(new Car());
+        Car car = new Car();
+        //When
+        Ticket ticket = smartParkingBoy.park(car);
+        //Then
+        assertNotNull(ticket);
+        Assertions.assertThat(systemOut()).contains("The car has parked in ParkingLot:2");
+    }
 
-       @Test
-       void should_with_error_msg_when_fetch_given_an_unrecognized_ticket(){
-           //Given
-           SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
-           ParkingLot firstParkingLot = new ParkingLot();
-           ParkingLot secondParkingLot = new ParkingLot();
-           smartParkingBoy.addParkingLot(firstParkingLot);
-           smartParkingBoy.addParkingLot(secondParkingLot);
-           Car car = new Car();
-           smartParkingBoy.park(car);
-           Ticket unrecognizedTicket = new Ticket();
-           //When
-           //Then
-           assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoy.fetch(unrecognizedTicket));
-       }
+    @Test
+    void should_return_right_car_when_fetch_given_a_parked_car_and_two_parking_tickets() {
+        //Given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        smartParkingBoy.addParkingLot(firstParkingLot);
+        smartParkingBoy.addParkingLot(secondParkingLot);
+        Car car = new Car();
+        Ticket firstTicket = smartParkingBoy.park(car);
+        Car firstFetchedCar = smartParkingBoy.fetch(firstTicket);
+        smartParkingBoy.park(new Car());
+        Ticket secondTicket = smartParkingBoy.park(car);
+        Car secondFetchedCar = smartParkingBoy.fetch(secondTicket);
+        //When
+        //Then
+        assert (car.equals(firstFetchedCar));
+        assert (car.equals(secondFetchedCar));
+    }
 
-       @Test
-       void should_with_error_msg_when_fetch_given_a_used_ticket(){
-           //Given
-           SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
-           ParkingLot firstParkingLot = new ParkingLot();
-           ParkingLot secondParkingLot = new ParkingLot();
-           smartParkingBoy.addParkingLot(firstParkingLot);
-           smartParkingBoy.addParkingLot(secondParkingLot);
-           Car car = new Car();
-           Ticket ticket = smartParkingBoy.park(car);
-           smartParkingBoy.fetch(ticket);
-           //When
-           //Then
-           assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoy.fetch(ticket));
-       }
+    @Test
+    void should_with_error_msg_when_fetch_given_an_unrecognized_ticket() {
+        //Given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        smartParkingBoy.addParkingLot(firstParkingLot);
+        smartParkingBoy.addParkingLot(secondParkingLot);
+        Car car = new Car();
+        smartParkingBoy.park(car);
+        Ticket unrecognizedTicket = new Ticket();
+        //When
+        //Then
+        assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoy.fetch(unrecognizedTicket));
+    }
+
+    @Test
+    void should_with_error_msg_when_fetch_given_a_used_ticket() {
+        //Given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        smartParkingBoy.addParkingLot(firstParkingLot);
+        smartParkingBoy.addParkingLot(secondParkingLot);
+        Car car = new Car();
+        Ticket ticket = smartParkingBoy.park(car);
+        smartParkingBoy.fetch(ticket);
+        //When
+        //Then
+        assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoy.fetch(ticket));
+    }
 
     @Test
     void should_with_error_msg_when_park_given_two_parking_lot_and_without_any_position() {
