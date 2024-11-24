333ba6a88eab2dc13cf731aa994e59e350a60271
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-24 12:47:05 +0800

    refactor:use stream instead of for loop

--- src/main/java/com/parkinglot/ParkingBoyStrategy.java
+++ src/main/java/com/parkinglot/ParkingBoyStrategy.java
@@ -5,14 +5,13 @@ import java.util.List;
 public class ParkingBoyStrategy implements ParkStrategy {
     @Override
     public Ticket park(List<ParkingLot> parkingLots, Car car) {
-        for (int i = 0; i < parkingLots.size(); i++) {
-            try {
-                Ticket ticket = parkingLots.get(i).park(car);
-                System.out.println("The car:" + car.getId() + " has parked in ParkingLot:" + (i + 1));
-                return ticket;
-            } catch (Exception e) {
-            }
+        ParkingLot parkingLot = parkingLots.stream()
+                .filter(lot -> lot.getNumberOfEmptyPositions() != 0)
+                .findFirst().orElse(null);
+        if (parkingLot == null) {
+            throw new NoAvailablePositionExpection();
         }
-        throw new NoAvailablePositionExpection();
+        System.out.println("The car:" + car.getId() + " has parked in ParkingLot:" + (parkingLots.indexOf(parkingLot) + 1));
+        return parkingLot.park(car);
     }
 }
